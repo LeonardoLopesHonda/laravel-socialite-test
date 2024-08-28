9a4155eaefede8ead82b50d9e76f8b0a21b2867a
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2024-08-28 12:28:51 -0400

    feat: :sparkles: First commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+// Route::get('/auth/redirect', function() {
+//     return Socialite::driver('github')->redirect();
+// });
+
+// Route::get('/auth/callback', function() {
+//     $user = Socialite::driver('github')->user();
+// });
